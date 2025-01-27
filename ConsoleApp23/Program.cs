0822dde10f8191956d8e6361e0955ce086f25627
chebanroman-sigma <r3388967@gmail.com>
2025-01-27 10:47:08 +0200

    задание 2
    задание 2

---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    internal class Program
+    {
+       static void Main(string[] args)
+       {    
+                Func();
+                Console.Read();
+       } 
+           static void Func()
+           {
+             int x, y;
+               for (x = 1; x <= 4; x++)
+               {
+                   for (y = 1; y <= 7; y++)
+                   {
+                     Console.Write("#");
+                   }
+                 Console.WriteLine($"  ");
+                 Console.WriteLine($"  ");
+               }
+           }
+    }    
+}
