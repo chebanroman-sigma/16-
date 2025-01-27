e18019a30c6240e082512e0669060037d128f69e
chebanroman-sigma <r3388967@gmail.com>
2025-01-27 11:23:47 +0200

    Задание 4
    Задание 4

---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.Read();
+        }
+
+        static void Func()
+        {
+            int x = 8;
+            for (int y = 0; y <= x; y++)
+            {
+                Console.Write(new string(' ', y));
+                Console.WriteLine("#");
+            }
+        }
+    }
+}
