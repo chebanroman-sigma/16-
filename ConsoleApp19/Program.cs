174b83b1c0b547fc1b96cd7ff75a6e3ae581fd25
chebanroman-sigma <r3388967@gmail.com>
2025-01-27 10:33:40 +0200

    задание 1 Б
    задание 1 Б

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.Read();
+        }
+
+        static void Func()
+        {
+            int x, a, y, value;
+            for (x = 4; x <= 7; x++)
+            {
+                for (a = 1; a <= 10; a++)
+                {
+                    y = x * 10;
+                    value = y + a;
+                    Console.Write($"{value} ");
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
+    
+
