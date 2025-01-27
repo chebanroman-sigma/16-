2895c14ebf8f4d39daef0b16525f963024cb281d
chebanroman-sigma <r3388967@gmail.com>
2025-01-27 10:29:13 +0200

    1 А
    1 А

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Func();
+                Console.Read();
+            }
+        }
+        static void Func()
+        {
+            for (int x = 1; x <= 4; x++)
+            {
+                for (int a = 1; a <= 10; a++)
+                {
+                    Console.Write($"{a} ");
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
