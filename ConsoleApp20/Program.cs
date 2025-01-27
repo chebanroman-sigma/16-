2748e6ca4b9daa3e45d6241dc35fa678e3cbb0fe
chebanroman-sigma <r3388967@gmail.com>
2025-01-27 10:36:07 +0200

    Задание 1 В
    Задание 1 В

---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                Func();
+                Console.Read();
+        }
+            static void Func()
+            {
+                int a = 5;
+                for (int x = 1; x <= 5; x++)
+                {
+                    for (int y = 1; y <= x; y++)
+                    {
+                        Console.Write("5 ");
+                    }
+                    Console.WriteLine(" ");
+                }
+            }
+    }
+}
